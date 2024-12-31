045c63d86c00f3d214cc59d76594307a510ef8b7
kawabatatakumi8901 <kawabatatakumi8901@github.com>
2024-12-31 19:58:43 +0700

    init

---
+++ b-o-o-k-w-a-l-k-e-r-t-w.meta.js
@@ -0,0 +1,13 @@
+// ==UserScript==
+// @name         b-o-o-k-w-a-l-k-e-r-t-w
+// @namespace    b-o-o-k-w-a-l-k-e-r-t-w.user.js
+// @version      2024.12.31
+// @description  down b-o-o-k-w-a-l-k-e-r-t-w
+// @author       You
+// @include      /^https:\/\/pcre[a@]der\.b[o0][o0]kw[a@]lker\.c[o0]m\.tw\x2F.+/
+// @icon         https://raw.githubusercontent.com/kawabatatakumi8901/khi_tra_trua/main/b-o-o-k-w-a-l-k-e-r-t-w.png
+// @grant        GM_download
+// @grant        GM_info
+// @downloadURL  https://github.com/kawabatatakumi8901/khi_tra_trua/raw/main/b-o-o-k-w-a-l-k-e-r-t-w.user.js
+// @updateURL    https://github.com/kawabatatakumi8901/khi_tra_trua/raw/main/b-o-o-k-w-a-l-k-e-r-t-w.meta.js
+// ==/UserScript==
